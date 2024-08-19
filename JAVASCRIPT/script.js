ee9a344ac16e943dba7b89fe91f5fdbd56896570
Hamdy-Tamer <hamdytamer253@gmail.com>
2024-08-19 22:23:11 +0300

    Weather-Web
     it is a simple and stylish weather forecasting web application that displays hourly weather updates, current location, temperature, and weather conditions. The application adjusts the temperature dynamically throughout the day, simulating a realistic weather pattern where temperatures rise in the morning and decrease after the afternoon.

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,91 @@
+// Global variables for API Locations.
+let latitude;
+let longitude;
+
+//Get Data form API
+async function getData() {
+    if (latitude === undefined || longitude === undefined) {
+        console.error("Error!\nLatitude or Longitude is not defined.");
+        return null;
+    }
+
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=eda8d98890214bab926190059241708&q=${latitude},${longitude}&hour=24&days=3`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.log(response);
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+        return json;
+    } catch (error) {
+        console.error(error.message);
+        return null;
+    }
+}
+
+
+function successCallback(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    fetchAndUpdateWeather();
+}
+
+function errorCallback(error) {
+    console.error(`Error: ${error.message}`);
+}
+
+async function fetchAndUpdateWeather() {
+    const data = await getData();
+    if (data) {
+        // DOM content
+        const currentHourElem = document.querySelector('.current-hour');
+        const currentDegreeElem = document.querySelector('.current-degree');
+        const dateRes = document.querySelector('.date');
+        const locationRes = document.querySelector('.location');
+        const dayRes = document.querySelector('.day');
+        const conditionRes = document.querySelector('.status');
+        const temperatureRes = document.querySelector('.temperature');
+
+        // Convert date format and days
+        const date = new Date();
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formatter = new Intl.DateTimeFormat('en-US', options);
+        const formattedDate = formatter.format(date);
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const dayIndex = date.getDay();
+        const dayName = daysOfWeek[dayIndex];
+
+        // Extract current hour (only hour part)
+        const currentHour = new Date().toLocaleTimeString([], { hour: '2-digit' });
+
+        // Round temperature to nearest number
+        const roundedTemperature = Math.round(data.current.temp_c);
+
+        // Apply New Values
+        if (dateRes && locationRes && dayRes && conditionRes && temperatureRes) {
+            dateRes.textContent = formattedDate;
+            locationRes.textContent = `${data.location.name}, ${data.location.country}`;
+            dayRes.textContent = dayName;
+            conditionRes.textContent = data.current.condition.text;
+            temperatureRes.textContent = `${roundedTemperature}°C`;
+            currentHourElem.textContent = currentHour;
+            currentDegreeElem.textContent = `${roundedTemperature}°C`;
+        }
+    }
+}
+
+// Get User Location
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (latitude !== undefined && longitude !== undefined) {
+        fetchAndUpdateWeather();
+    }
+});
